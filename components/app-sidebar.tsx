b43f1692227038b94d65addbc0698d5e1619d886
Raxuis <raclot.raphael@gmail.com>
2025-01-26 16:21:54 +0100

    feat(vuejs): add vuejs installation

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,114 +1,111 @@
-"use client"
+"use client";
 
-import * as React from "react"
+import * as React from "react";
 import {
-    BookMarkedIcon,
-    Fingerprint,
-    Home,
-    Navigation, PenLineIcon,
-} from "lucide-react"
-import {FaReact} from "react-icons/fa6";
-import {FaVuejs} from "react-icons/fa";
-import {NavLanguages} from "@/components/nav-languages"
+  BookMarkedIcon,
+  Fingerprint,
+  Home,
+  Navigation,
+  PenLineIcon,
+} from "lucide-react";
+import { FaReact } from "react-icons/fa6";
+import { FaVuejs } from "react-icons/fa";
+import { NavLanguages } from "@/components/nav-languages";
 
-import {
-    Sidebar,
-    SidebarContent,
-    SidebarRail,
-} from "@/components/ui/sidebar"
-import {NavWebsite} from "@/components/nav-website";
-import {RiNextjsFill} from "react-icons/ri";
-import {RiTailwindCssFill} from "react-icons/ri";
+import { Sidebar, SidebarContent, SidebarRail } from "@/components/ui/sidebar";
+import { NavWebsite } from "@/components/nav-website";
+import { RiNextjsFill } from "react-icons/ri";
+import { RiTailwindCssFill } from "react-icons/ri";
 
 const data = {
-    navLanguages: [
+  navLanguages: [
+    {
+      title: "Nextjs",
+      url: "/docs/nextjs",
+      icon: RiNextjsFill,
+      isActive: true,
+      items: [
         {
-            title: "Nextjs",
-            url: "/docs/nextjs",
-            icon: RiNextjsFill,
-            isActive: true,
-            items: [
-                {
-                    title: "Installation",
-                    url: "/docs/nextjs/installation",
-                },
-            ],
+          title: "Installation",
+          url: "/docs/nextjs/installation",
+        },
+      ],
+    },
+    {
+      title: "Reactjs",
+      url: "/docs/reactjs",
+      icon: FaReact,
+      items: [
+        {
+          title: "Installation",
+          url: "/docs/reactjs/installation",
+        },
+      ],
+    },
+    {
+      title: "Vuejs",
+      url: "/docs/vuejs",
+      icon: FaVuejs,
+      items: [
+        {
+          title: "Installation",
+          url: "/docs/vuejs/installation",
+        },
+      ],
+    },
+    {
+      title: "Tailwindcss",
+      url: "/docs/tailwindcss",
+      icon: RiTailwindCssFill,
+      items: [
+        {
+          title: "Installation",
+          url: "/docs/tailwindcss/installation",
+        },
+      ],
+    },
+  ],
+  navigation: [
+    {
+      title: "Navigation",
+      url: "#",
+      icon: Navigation,
+      isActive: true,
+      items: [
+        {
+          title: "Home",
+          url: "/",
+          icon: Home,
         },
         {
-            title: "Reactjs",
-            url: "/docs/reactjs",
-            icon: FaReact,
-            items: [
-                {
-                    title: "Installation",
-                    url: "/docs/reactjs/installation",
-                },
-            ],
+          title: "About",
+          url: "/about",
+          icon: Fingerprint,
         },
         {
-            title: "Vuejs",
-            url: "/docs/vuejs",
-            icon: FaVuejs,
-            items: [
-                {
-                    title: "Installation",
-                    url: "/docs/reactjs/installation",
-                },
-            ],
+          title: "Documentations",
+          url: "/docs",
+          icon: BookMarkedIcon,
         },
         {
-            title: "Tailwindcss",
-            url: "/docs/tailwindcss",
-            icon: RiTailwindCssFill,
-            items: [
-                {
-                    title: "Installation",
-                    url: "/docs/tailwindcss/installation",
-                },
-            ],
-        }
-    ],
-    navigation: [
-        {
-            title: "Navigation",
-            url: "#",
-            icon: Navigation,
-            isActive: true,
-            items: [
-                {
-                    title: "Home",
-                    url: "/",
-                    icon: Home
-                },
-                {
-                    title: "About",
-                    url: "/about",
-                    icon: Fingerprint
-                },
-                {
-                    title: "Documentations",
-                    url: "/docs",
-                    icon: BookMarkedIcon
-                },
-                {
-                    title: "Contributing",
-                    url: "https://github.com/Raxuis/code-with-raf",
-                    outsideLink: true,
-                    icon: PenLineIcon
-                }
-            ],
+          title: "Contributing",
+          url: "https://github.com/Raxuis/code-with-raf",
+          outsideLink: true,
+          icon: PenLineIcon,
         },
-    ],
-}
+      ],
+    },
+  ],
+};
 
-export function AppSidebar({...props}: React.ComponentProps<typeof Sidebar>) {
-    return (
-        <Sidebar collapsible="icon" {...props}>
-            <SidebarContent>
-                <NavLanguages items={data.navLanguages}/>
-                <NavWebsite items={data.navigation}/>
-            </SidebarContent>
-            <SidebarRail/>
-        </Sidebar>
-    )
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarContent>
+        <NavLanguages items={data.navLanguages} />
+        <NavWebsite items={data.navigation} />
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
 }
